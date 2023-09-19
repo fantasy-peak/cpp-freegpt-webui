fd5fc22af41883362802a69b99c62e62d3476ab3
fantasy-peak <1356346239@qq.com>
2023-09-19 10:56:32 +0900

    Remove getGpt,opChatGpts,wuguokai,wuguokai

--- include/free_gpt.h
+++ include/free_gpt.h
@@ -20,17 +20,13 @@ public:
 
     boost::asio::awaitable<void> aiTianhu(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> deepAi(std::shared_ptr<Channel>, nlohmann::json);
-    boost::asio::awaitable<void> getGpt(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> aiChat(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> chatGptAi(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> weWordle(std::shared_ptr<Channel>, nlohmann::json);
-    boost::asio::awaitable<void> opChatGpts(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> acytoo(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> openAi(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> h2o(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> yqcloud(std::shared_ptr<Channel>, nlohmann::json);
-    boost::asio::awaitable<void> wuguokai(std::shared_ptr<Channel>, nlohmann::json);
-    boost::asio::awaitable<void> liaobots(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> huggingChat(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> you(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> binjie(std::shared_ptr<Channel>, nlohmann::json);
