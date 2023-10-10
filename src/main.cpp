e5601a01dc43dd2310c51bd56c2da15c03651583
fantasy-peak <1356346239@qq.com>
2023-10-10 16:11:21 +0800

    Add provider chatGptDemo

--- src/main.cpp
+++ src/main.cpp
@@ -350,6 +350,7 @@ int main(int argc, char** argv) {
     ADD_METHOD("gpt-3.5-turbo-stream-gptalk", FreeGpt::gptalk);
     ADD_METHOD("gpt-3.5-turbo-stream-ChatForAi", FreeGpt::chatForAi);
     ADD_METHOD("gpt-3.5-turbo-stream-gptforlove", FreeGpt::gptForLove);
+    ADD_METHOD("gpt-3.5-turbo-stream-ChatgptDemo", FreeGpt::chatGptDemo);
 
     SPDLOG_INFO("active provider:");
     for (auto& [provider, _] : gpt_function)
