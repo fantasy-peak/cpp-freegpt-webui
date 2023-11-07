fd2c56dd106ffe0768a533a14dd10c334c4a4b2e
fantasy-peak <1356346239@qq.com>
2023-11-07 15:31:36 +0800

    Update provider aivvm && you

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,7 @@ add_repositories("my_private_repo https://github.com/fantasy-peak/xmake-repo.git
 
 add_requires("openssl", {system = false})
 add_requires("zlib", {system = false})
-add_requires("yaml_cpp_struct", "nlohmann_json", "spdlog", "inja", "plusaes")
+add_requires("yaml_cpp_struct", "nlohmann_json", "spdlog", "inja", "plusaes", "concurrentqueue")
 add_requires("boost", {configs = {iostreams = true}})
 
 set_languages("c++23")
@@ -17,6 +17,6 @@ add_includedirs("include")
 target("cpp-freegpt-webui")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_packages("openssl", "yaml_cpp_struct", "nlohmann_json", "spdlog", "boost", "inja", "plusaes", "zlib")
+    add_packages("openssl", "yaml_cpp_struct", "nlohmann_json", "spdlog", "boost", "inja", "plusaes", "zlib", "concurrentqueue")
     add_syslinks("pthread", "curl-impersonate-chrome")
 target_end()
