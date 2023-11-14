e3c4862e5db540aef24ae9819f35c0c40c5ea650
fantasy-peak <1356346239@qq.com>
2023-11-14 09:35:09 +0800

    Add provider Berlin

--- include/free_gpt.h
+++ include/free_gpt.h
@@ -38,6 +38,7 @@ public:
     boost::asio::awaitable<void> fakeGpt(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> vercel(std::shared_ptr<Channel>, nlohmann::json);
     boost::asio::awaitable<void> aivvm(std::shared_ptr<Channel>, nlohmann::json);
+    boost::asio::awaitable<void> berlin(std::shared_ptr<Channel>, nlohmann::json);
 
 private:
     boost::asio::awaitable<std::expected<boost::beast::ssl_stream<boost::beast::tcp_stream>, std::string>>
